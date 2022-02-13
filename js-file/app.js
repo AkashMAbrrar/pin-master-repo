38f10e4c65bb432feef8e2513253f67ca686483b
AkashMAbrrar <mabrrar9@gmail.com>
2022-02-13 22:14:05 +0600

    success and faild alert are added for mcthcing or not

--- js-file/app.js
+++ js-file/app.js
@@ -12,15 +12,36 @@ function getPin() {
 
 function genaratePin() {
     const pin = getPin();
-    document.getElementById('display-input').value = pin;
+    document.getElementById('display-pin').value = pin;
 
 };
 
 document.getElementById('key-pad').addEventListener('click', function (event) {
     const number = event.target.innerText;
     const calcInput = document.getElementById('typed-numbers');
-    const previousNumber = calcInput.value;
-    const newNumber = previousNumber + number;
-    calcInput.value = newNumber;
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
 
-})
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+
+});
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    if (pin == typedNumbers) {
+        const successMessage = document.getElementById('notify-success');
+        successMessage.style.display = 'block';
+    }
+    else {
+        const failError = document.getElementById('notify-fail');
+        failError.style.display = 'block';
+    }
+}
